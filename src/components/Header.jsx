01c3b439a49cfa94ee440495d77fd5141104beca
woonbeap <woonbeap@student.21-school.ru>
2025-01-27 17:27:44 +0300

    first layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import style from './Header.module.css'
 
 const Header = () => {
     return (
-        <header className='header'>
-            <img src="https://www.seekpng.com/png/full/6-64737_orange-clipart-speech-bubble-bubble-chat-png.png" alt="logo"/>
+        <header className={style.header}>
+            <div className={style.logoContainer}>
+                <img src="https://www.seekpng.com/png/full/6-64737_orange-clipart-speech-bubble-bubble-chat-png.png" alt="logo"/>
+            </div>
         </header>)
 }
 
