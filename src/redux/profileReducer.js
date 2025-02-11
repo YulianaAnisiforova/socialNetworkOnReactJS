597ae03d6cbfcf3d84461b506b8c5959922ed110
woonbeap <yulianaanisiforova@gmail.com>
2025-02-11 11:07:17 +0300

    some refactoring

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -3,7 +3,12 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 
 let initialState = {
     posts: [
-        {id: 0, message: 'Hello everyone it`s my first post here haha', likes: '7'},
+        {
+            id: 3,
+            message: 'I`m going to visit my gruncle Stan again this summer OMG soooo excited!! Looking forward to meet him again!!!!!',
+            likes: '1488'
+        },
+        {id: 2, message: 'My brother Dipper is sooooo annoying', likes: '2'},
         {
             id: 1, message: 'la la la la la la la la la la la la la la la la\n' +
                 'la la la la la la la la la la la la la la la la\n' +
@@ -14,12 +19,7 @@ let initialState = {
                 'la la la la la la la la la la la la la la la la\n' +
                 'la la la la la la la la la la la la la la la la', likes: '0'
         },
-        {id: 2, message: 'My brother Dipper is sooooo annoying', likes: '2'},
-        {
-            id: 3,
-            message: 'I`m going to visit my gruncle Stan again this summer OMG soooo excited!! Looking forward to meet him again!!!!!',
-            likes: '1488'
-        },
+        {id: 0, message: 'Hello everyone it`s my first post here haha', likes: '7'},
     ],
     newPostText: '',
 }
@@ -29,7 +29,7 @@ const profileReducer = (state = initialState, action) => {
         case ADD_POST:
             return {
                 ...state,
-                posts: [...state.posts, {id: state.posts.length, message: state.newPostText, likesCount: 0,}],
+                posts: [{id: state.posts.length, message: state.newPostText, likesCount: 0,}, ...state.posts],
                 newPostText: '',
             }
         case UPDATE_NEW_POST_TEXT:
