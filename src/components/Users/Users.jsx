9a83df330997852e4dfbdb5c3286407388ce8f13
woonbeap <yulianaanisiforova@gmail.com>
2025-02-11 12:10:03 +0300

    getUsers func

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,32 +4,39 @@ import axios from "axios";
 import anonymousAvatar from './../../img/cipher.WEBP'
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        })
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items)
+            })
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>get users</button>
             {
                 props.users.map((user) => <div key={user.id} className={style.item}>
-                    <div className={style.avatarFollow}>
-                        <div>
-                            <img className={style.userAvatar} src={user.photos.small != null
-                                ? user.photos.small : anonymousAvatar} alt="avatar"/>
+                        <div className={style.avatarFollow}>
+                            <div>
+                                <img className={style.userAvatar} src={user.photos.small != null
+                                    ? user.photos.small : anonymousAvatar} alt="avatar"/>
+                            </div>
+                            <div>
+                                {user.followed ?
+                                    <button className={style.followBtn} onClick={() => {
+                                        props.unfollowUser(user.id)
+                                    }}>unfollow</button>
+                                    : <button className={style.followBtn} onClick={() => {
+                                        props.followUser(user.id)
+                                    }}>follow</button>}
+                            </div>
                         </div>
-                        <div>
-                            {user.followed ?
-                                <button className={style.followBtn} onClick={() => {props.unfollowUser(user.id)}}>unfollow</button>
-                                : <button className={style.followBtn} onClick={() => {props.followUser(user.id)}}>follow</button>}
+                        <div className={style.userInfo}>
+                            <div>{user.name}</div>
+                            <div>{user.status}</div>
+                            <div>{'user.location.country'}, {'user.location.city'}</div>
                         </div>
-                    </div>
-                    <div className={style.userInfo}>
-                        <div>{user.name}</div>
-                        <div>{user.status}</div>
-                        <div>{'user.location.country'}, {'user.location.city'}</div>
-                    </div>
 
                     </div>
                 )
