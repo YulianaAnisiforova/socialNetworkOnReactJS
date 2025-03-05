546c42e13df64f24dad45d6435e726ec307d2758
Woonbeam Post <yulianaanisiforova@gmail.com>
2025-03-05 14:30:24 +0300

    React.lazy и React.Suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import './App.css'
-import NavBar from './components/NavBar/NavBar'
 import {Navigate, Route, Routes} from 'react-router-dom'
-import DialogsContainer from './components/Dialogs/DialogsContainer'
-import UsersContainer from './components/Users/UsersContainer'
-import ProfileContainer from './components/Profile/ProfileContainer'
+import NavBar from './components/NavBar/NavBar'
 import HeaderComponent from './components/Header/HeaderContainer'
-import Login from './components/Login/Login'
+import Preloader from './components/Common/Preloader/Preloader'
+// import DialogsContainer from './components/Dialogs/DialogsContainer'
+// import ProfileContainer from './components/Profile/ProfileContainer'
+// import UsersContainer from './components/Users/UsersContainer'
+// import Login from './components/Login/Login'
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'))
+const Login = React.lazy(() => import('./components/Login/Login'))
 
 const App = () => {
     return (
@@ -14,13 +20,15 @@ const App = () => {
             <HeaderComponent/>
             <NavBar/>
             <div className='appWrapperContent'>
-                <Routes>
-                    <Route path='/' element={<Navigate to='/login'/>} />
-                    <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
-                    <Route path='/users' element={<UsersContainer/>}/>
-                    <Route path='/dialogs/*' element={<DialogsContainer/>}/>
-                    <Route path='/login' element={<Login/>}/>
-                </Routes>
+                <Suspense fallback={<Preloader/>}>
+                    <Routes>
+                        <Route path='/' element={<Navigate to='/login'/>}/>
+                        <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
+                        <Route path='/users' element={<UsersContainer/>}/>
+                        <Route path='/dialogs/*' element={<DialogsContainer/>}/>
+                        <Route path='/login' element={<Login/>}/>
+                    </Routes>
+                </Suspense>
             </div>
         </div>
     )
